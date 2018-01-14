0000023212f523753e8d4d5e44937bb2cb0f1c93
Pierre Hersant <phersant@protonmail.com>
2018-01-14 15:45:16 +0100

    add validation on course title

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates_presence_of :title' do
+    let(:course) { Course.new }
+
+    it 'will refuse validation if no title' do
+      expect(course.save). to be false
+      expect(course.errors.messages).to eql(title: ["can't be blank"])
+    end
+
+    it 'will accept validation with some title' do
+      course.title = 'awesome course title'
+      expect(course.save).to be true
+    end
+  end
 end
