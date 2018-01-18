0928ddf4c979e285a79a02ab0c5073117e7fd46b
Pierre Hersant <phersant@protonmail.com>
2018-01-18 21:09:35 +0100

    add position in course validation

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -2,4 +2,11 @@ class Chapter < ApplicationRecord
   belongs_to :course
   has_many :units
   validates_presence_of :title
+  validates_presence_of :position_in_course
+  validate :unique_position_in_course
+
+  def unique_position_in_course
+    return unless self.course.chapters.pluck(:position_in_course).include?(self.position_in_course)
+    errors.add(:position_in_course, 'already taken')
+  end
 end
--- spec/models/chapter_spec.rb
+++ spec/models/chapter_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Chapter, type: :model do
-    describe 'validates_presence_of :title' do
+  describe 'validations' do
     let!(:course) { Course.new(title: 'awesome title') }
-    let(:chapter) { Chapter.new(course: course) }
+    let(:chapter) { Chapter.new(course: course, position_in_course: 1) }
 
     it 'will refuse validation if no title' do
       expect(chapter.save). to be false
@@ -13,6 +13,13 @@ RSpec.describe Chapter, type: :model do
     it 'will accept validation with some title' do
       chapter.title = 'awesome chapter title'
       expect(chapter.save).to be true
+    end
+
+    it 'will not save if position_in_course is already taken' do
+      Chapter.create(course: course, title: 'title', position_in_course: 1)
+      chapter.title = 'title'
+      expect(chapter.save).to be false
+      expect(chapter.errors.messages).to eql(position_in_course: ['already taken'])
     end
   end
 end
