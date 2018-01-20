37da5f4eb52a72d76764c90b4d445a6c427eaf68
Pierre Hersant <phersant@protonmail.com>
2018-01-20 14:20:50 +0100

    add validations on unit example

--- app/models/unit_example.rb
+++ app/models/unit_example.rb
@@ -2,4 +2,10 @@ class UnitExample < ApplicationRecord
   belongs_to :unit
   validates_presence_of :content
   validates_presence_of :position_in_unit
+  validate :unique_position_in_unit
+
+  def unique_position_in_unit
+    return unless self.unit.unit_examples.pluck(:position_in_unit).include?(self.position_in_unit)
+    errors.add(:position_in_unit, 'already taken')
+  end
 end
--- spec/models/unit_example_spec.rb
+++ spec/models/unit_example_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UnitExample, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:course) { Course.create(title: 'awesome title') }
+    let(:chapter) { Chapter.create(course: course, position_in_course: 1, title: 'title') }
+    let(:unit) { Unit.create(chapter: chapter, position_in_chapter: 1, title: 'title') }
+    let(:unit_example) { UnitExample.new(unit: unit, content: 'content', position_in_unit: 1) }
+
+    it 'will not save if position_in_unit is already taken' do
+      UnitExample.create(unit: unit, position_in_unit: 1, content: 'content')
+
+      expect(unit_example.save).to be false
+      expect(unit_example.errors.messages).to eql(position_in_unit: ['already taken'])
+    end
+  end
 end
