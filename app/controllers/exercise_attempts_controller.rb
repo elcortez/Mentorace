1a08b1b8619131be76940fef1b9af23af5c14c63
Pierre Hersant <phersant@protonmail.com>
2018-01-25 21:57:56 +0100

    wip - how to render form within show / OR render show as partial, within form?

--- app/controllers/exercise_attempts_controller.rb
+++ app/controllers/exercise_attempts_controller.rb
@@ -1,5 +1,10 @@
 class ExerciseAttemptsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
-    @exercise_attempt = ExerciseAttempt.new
+    @exercise_attempt = ExerciseAttempt.new(
+      user_id: current_user.id,
+      unit_exercise_id: @unit_exercise.id
+    )
   end
 end
