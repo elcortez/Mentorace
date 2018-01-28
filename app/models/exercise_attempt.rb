1f9c5bf8a162b4a6996aefa7790f7f59ecf89f3c
Pierre Hersant <phersant@protonmail.com>
2018-01-28 17:52:48 +0100

    set successful

--- app/models/exercise_attempt.rb
+++ app/models/exercise_attempt.rb
@@ -1,4 +1,9 @@
 class ExerciseAttempt < ApplicationRecord
   belongs_to :user
   belongs_to :unit_exercise
+  before_validation :set_successful
+
+  def set_successful
+    self.attempt_successful = self.attempted_answer == self.unit_exercise.answer
+  end
 end
--- spec/models/exercise_attempt_spec.rb
+++ spec/models/exercise_attempt_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe ExerciseAttempt, type: :model do
+  describe '#set_successful' do
+    let!(:unit_exercise) { create(:unit_exercise,
+      question: double('question'),
+      unit: create(:unit, chapter: create(:chapter, course: create(:course))),
+      answer: '123'
+    ) }
+
+    let!(:user) { create(:user) }
+    let!(:successful_attempt) { create(:exercise_attempt,
+      unit_exercise: unit_exercise, attempted_answer: '123', user: user) }
+
+    let!(:failed_attempt) { create(:exercise_attempt,
+      unit_exercise: unit_exercise, attempted_answer: '1234', user: user) }
+
+    it 'sets successful and failure on create' do
+      expect(successful_attempt.attempt_successful).to eql(true)
+      expect(failed_attempt.attempt_successful).to eql(false)
+    end
+  end
 end
