39dc73b684c07755e00fc2e7b819431d418601f0
Pierre Hersant <phersant@protonmail.com>
2018-01-28 16:56:06 +0100

    route to new exercise

--- app/controllers/exercise_attempts_controller.rb
+++ app/controllers/exercise_attempts_controller.rb
@@ -2,9 +2,11 @@ class ExerciseAttemptsController < ApplicationController
   before_action :authenticate_user!
 
   def new
+    @unit_exercise = UnitExercise.find(params[:unit_exercise_id])
+
     @exercise_attempt = ExerciseAttempt.new(
-      user_id: current_user.id,
-      unit_exercise_id: @unit_exercise.id
+      unit_exercise_id: @unit_exercise.id,
+      user_id: current_user.id
     )
   end
 end
--- app/controllers/unit_exercises_controller.rb
+++ app/controllers/unit_exercises_controller.rb
@@ -1,6 +1,3 @@
 class UnitExercisesController < ApplicationController
   before_action :authenticate_user!
-  def show
-    @unit_exercise = UnitExercise.find(params[:id])
-  end
 end
