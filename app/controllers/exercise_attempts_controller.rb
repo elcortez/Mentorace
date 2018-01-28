a102925206911aa858c3bc0addc272e52146abde
Pierre Hersant <phersant@protonmail.com>
2018-01-28 17:12:19 +0100

    redirect back fallback route

--- app/controllers/exercise_attempts_controller.rb
+++ app/controllers/exercise_attempts_controller.rb
@@ -1,12 +1,32 @@
 class ExerciseAttemptsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_unit_exercise
 
   def new
-    @unit_exercise = UnitExercise.find(params[:unit_exercise_id])
+  end
 
-    @exercise_attempt = ExerciseAttempt.new(
-      unit_exercise_id: @unit_exercise.id,
-      user_id: current_user.id
-    )
+  def create
+    fallback_params = {
+      course_id: @unit.chapter.course_id,
+      chapter_id: @unit.chapter_id,
+      unit_id: @unit.id,
+      unit_exercise_id: @unit.unit_exercises.first.id
+    }
+
+    p params
+    # ExerciseAttempt.create!(
+    #   unit_exercise_id: @unit_exercise.id,
+    #   user_id: current_user.id,
+    #   attempted_answer: params['exercise_attempt']['attempted_answer']
+    # )
+
+    redirect_back fallback_location: new_course_chapter_unit_unit_exercise_exercise_attempt_path(fallback_params)
+  end
+
+  private
+
+  def find_unit_exercise
+    @unit_exercise = UnitExercise.find(params[:unit_exercise_id])
+    @unit = @unit_exercise.unit
   end
 end
