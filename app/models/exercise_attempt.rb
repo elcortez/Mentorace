affd64f11a83b19ee0d66f488f0f4e6ab2ec8f23
Pierre Hersant <phersant@protonmail.com>
2018-02-12 21:05:55 +0100

    first draft update_learning_status

--- app/models/exercise_attempt.rb
+++ app/models/exercise_attempt.rb
@@ -2,8 +2,13 @@ class ExerciseAttempt < ApplicationRecord
   belongs_to :user
   belongs_to :unit_exercise
   before_validation :set_successful
+  after_create :update_learning_status, if: -> (e) { e.attempt_successful? }
 
   def set_successful
     self.attempt_successful = self.attempted_answer.present? && self.attempted_answer == self.unit_exercise.answer
+  end
+
+  def update_learning_status
+    
   end
 end
--- spec/models/exercise_attempt_spec.rb
+++ spec/models/exercise_attempt_spec.rb
@@ -20,4 +20,42 @@ RSpec.describe ExerciseAttempt, type: :model do
       expect(failed_attempt.attempt_successful).to eql(false)
     end
   end
+
+  describe '#update_learning_status' do
+    let!(:course) { create(:course) }
+    let!(:chapter) { create(:chapter, course: course) }
+    let!(:unit) { create(:unit, chapter: chapter) }
+    let!(:unit_exercise) { create(:unit_exercise, unit: unit, position_in_unit: 1) }
+    let!(:unit_exercise_2) { create(:unit_exercise, unit: unit, position_in_unit: 2) }
+
+    let!(:user) { create(:user) }
+
+    it 'will update learning status if exercise attempt is successful' do
+      expect(user.learning_statuses.count).to eql(1)
+      expect(user.learning_statuses.first.attributes
+        .except('created_at', 'id', 'updated_at')
+        .to eql(
+          'user_id' => user.id,
+          'course_id' => course.id,
+          'chapter_id' => chapter.id,
+          'unit_id' => unit.id,
+          'unit_exercise_id' => unit_exercise.id
+        )
+      )
+
+      create(:exercise_attempt, user: user, unit_exercise: unit_exercise, attempted_answer: unit_exercise.answer)
+      user.reload
+
+      expect(user.learning_statuses.first.attributes
+        .except('created_at', 'id', 'updated_at')
+        .to eql(
+          'user_id' => user.id,
+          'course_id' => course.id,
+          'chapter_id' => chapter.id,
+          'unit_id' => unit.id,
+          'unit_exercise_id' => unit_exercise_2.id
+        )
+      )
+    end
+  end
 end
