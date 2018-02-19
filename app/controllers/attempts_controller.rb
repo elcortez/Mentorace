43d7b265ce8d97929c9fcda9f3c8f9d9994c07e9
Pierre Hersant <phersant@protonmail.com>
2018-02-19 22:55:06 +0100

    redirect if user cannot access exercise

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -1,9 +1,20 @@
 class AttemptsController < ApplicationController
   before_action :authenticate_user!
-  before_action :find_exercise
+  before_action :find_objects
 
   def new
-    @attempt = Attempt.new
+    if current_user.can_access_exercise?(@exercise)
+      @attempt = Attempt.new
+    else
+      status = LearningStatus.find_by(user_id: current_user.id, course_id: @course.id)
+
+      redirect_to new_course_chapter_unit_exercise_attempt_path(
+        course_id: status.course_id,
+        chapter_id: status.chapter_id,
+        unit_id: status.unit_id,
+        exercise_id: status.exercise_id
+      )
+    end
   end
 
   def create
@@ -19,8 +30,10 @@ class AttemptsController < ApplicationController
 
   private
 
-  def find_exercise
+  def find_objects
     @exercise = Exercise.find(params[:exercise_id])
     @unit = @exercise.unit
+    @chapter = @unit.chapter
+    @course = @chapter.course
   end
 end
--- spec/controllers/attempts_controller_spec.rb
+++ spec/controllers/attempts_controller_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe AttemptsController, type: :controller do
   describe 'new / create' do
-    let!(:user) { create(:user) }
     let!(:course) { create(:course) }
     let!(:chapter) { create(:chapter, course: course) }
     let!(:unit) { create(:unit, chapter: chapter) }
@@ -10,32 +9,99 @@ RSpec.describe AttemptsController, type: :controller do
     let!(:exercise_2) { create(:exercise, unit: unit, position_in_unit: 2) }
     let!(:exercise_3) { create(:exercise, unit: unit, position_in_unit: 3) }
 
+    let!(:user) { create(:user) }
+    let!(:user_two) { create(:user, email: 'other@gmail.com') }
+
     before do
       allow(controller).to receive(:authenticate_user!)
-      allow(controller).to receive(:current_user) { current_user }
+      allow(controller).to receive(:current_user) { user }
     end
 
     it 'will always redirect to the first unit exercise that has not yet been validated' do
+      response = get :new, params: {
+        course_id: course.id,
+        chapter_id: chapter.id,
+        unit_id: unit.id,
+        exercise_id: exercise_2.id
+      }
 
-      # response = get :new, params: {
-      #   course_id: course.id,
-      #   chapter_id: chapter.id,
-      #   unit_id: unit.id,
-      #   exercise_id: exercise_2.id
-      # }
-      #
-      # p response
-      #
-      # Parameters: {"course_id"=>"1", "chapter_id"=>"1", "unit_id"=>"2", "exercise_id"=>"4"}
+      expect(response).to redirect_to new_course_chapter_unit_exercise_attempt_path(
+        course_id: course.id,
+        chapter_id: chapter.id,
+        unit_id: unit.id,
+        exercise_id: exercise.id
+      )
+    end
 
-      # expect(response).to redirect_to(:back)
-      # expect(response).to redirect_to new_course_chapter_unit_exercise_attempt_path(
-      #   course_id: course.id,
-      #   chapter_id: chapter.id,
-      #   unit_id: unit.id,
-      #   exercise_id: exercise.id
-      #
-      # )
+    it 'will redirect to the first unit exercise that has not yet been validated' do
+      response = get :new, params: {
+        course_id: course.id,
+        chapter_id: chapter.id,
+        unit_id: unit.id,
+        exercise_id: exercise_3.id
+      }
+
+      expect(response).to redirect_to new_course_chapter_unit_exercise_attempt_path(
+        course_id: course.id,
+        chapter_id: chapter.id,
+        unit_id: unit.id,
+        exercise_id: exercise.id
+      )
+    end
+
+    it 'will redirect to the first unit exercise that has not yet been validated even if another one is validated' do
+      create(:attempt, user: user, exercise: exercise, attempted_answer: exercise.answer)
+
+      response = get :new, params: {
+        course_id: course.id,
+        chapter_id: chapter.id,
+        unit_id: unit.id,
+        exercise_id: exercise_3.id
+      }
+
+      expect(response).to redirect_to new_course_chapter_unit_exercise_attempt_path(
+        course_id: course.id,
+        chapter_id: chapter.id,
+        unit_id: unit.id,
+        exercise_id: exercise_2.id
+      )
+    end
+
+    it 'will render good view if it is the first exercise' do
+      response = get :new, params: {
+        course_id: course.id,
+        chapter_id: chapter.id,
+        unit_id: unit.id,
+        exercise_id: exercise.id
+      }
+
+      expect(response.code).to eql('200')
+    end
+
+    it 'will render good view if previous exercise has been validated' do
+      create(:attempt, user: user, exercise: exercise, attempted_answer: exercise.answer)
+      response = get :new, params: {
+        course_id: course.id,
+        chapter_id: chapter.id,
+        unit_id: unit.id,
+        exercise_id: exercise_2.id
+      }
+
+      expect(response.code).to eql('200')
+    end
+
+    it 'will render good view if all previous exercises have been validated' do
+      create(:attempt, user: user, exercise: exercise, attempted_answer: exercise.answer)
+      create(:attempt, user: user, exercise: exercise_2, attempted_answer: exercise_2.answer)
+
+      response = get :new, params: {
+        course_id: course.id,
+        chapter_id: chapter.id,
+        unit_id: unit.id,
+        exercise_id: exercise_3.id
+      }
+
+      expect(response.code).to eql('200')
     end
 
     # it 'in case of a post, it will save attempt && redirect to same exercise if it is a wrong answer' do
