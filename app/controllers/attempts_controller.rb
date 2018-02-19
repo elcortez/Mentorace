9f8bcac9ddcd8319bd256181b3c6ac0f9097256d
Pierre Hersant <phersant@protonmail.com>
2018-02-19 23:15:16 +0100

    refactor attempts controller

--- app/controllers/attempts_controller.rb
+++ app/controllers/attempts_controller.rb
@@ -3,12 +3,21 @@ class AttemptsController < ApplicationController
   before_action :find_objects
 
   def new
-    if current_user.can_access_exercise?(@exercise)
-      @attempt = Attempt.new
-    else
+    validate_user_presence
+    @attempt = Attempt.new
+  end
+
+  def create
+    validate_user_presence
+  end
+
+  private
+
+  def validate_user_presence
+    unless current_user.can_access_exercise?(@exercise)
       status = LearningStatus.find_by(user_id: current_user.id, course_id: @course.id)
 
-      redirect_to new_course_chapter_unit_exercise_attempt_path(
+      return redirect_to new_course_chapter_unit_exercise_attempt_path(
         course_id: status.course_id,
         chapter_id: status.chapter_id,
         unit_id: status.unit_id,
@@ -16,19 +25,6 @@ class AttemptsController < ApplicationController
       )
     end
   end
-
-  def create
-    fallback_params = {
-      course_id: @unit.chapter.course_id,
-      chapter_id: @unit.chapter_id,
-      unit_id: @unit.id,
-      exercise_id: @unit.exercises.first.id
-    }
-
-    redirect_back fallback_location: new_course_chapter_unit_exercise_attempt_path(fallback_params)
-  end
-
-  private
 
   def find_objects
     @exercise = Exercise.find(params[:exercise_id])
--- spec/controllers/attempts_controller_spec.rb
+++ spec/controllers/attempts_controller_spec.rb
@@ -23,53 +23,64 @@ RSpec.describe AttemptsController, type: :controller do
       allow(controller).to receive(:current_user) { user }
     end
 
-    it 'will always redirect to the first unit exercise that has not yet been validated' do
-      response = get :new, params: basic_params.merge(exercise_id: exercise_2.id)
-      expect(response).to redirect_to new_course_chapter_unit_exercise_attempt_path(
-        basic_params.merge(exercise_id: exercise.id)
-      )
+    describe 'create' do
+      it 'will redirect to same exercise if user should not have access to exercise' do
+      end
+
+      it 'it will save && redirect to same exercise if it is a wrong answer' do
+      end
+
+      it 'will redirect to the next exercise if attempt is successful' do
+      end
     end
 
-    it 'will redirect to the first unit exercise that has not yet been validated' do
-      response = get :new, params: basic_params.merge(exercise_id: exercise_3.id)
-      expect(response).to redirect_to new_course_chapter_unit_exercise_attempt_path(
-        basic_params.merge(exercise_id: exercise.id)
-      )
+    describe 'new' do
+      it 'will always redirect to the first unit exercise that has not yet been validated' do
+        response = get :new, params: basic_params.merge(exercise_id: exercise_2.id)
+        expect(response).to redirect_to new_course_chapter_unit_exercise_attempt_path(
+          basic_params.merge(exercise_id: exercise.id)
+        )
+      end
+
+      it 'will redirect to the first unit exercise that has not yet been validated even if exercise is further away' do
+        response = get :new, params: basic_params.merge(exercise_id: exercise_3.id)
+        expect(response).to redirect_to new_course_chapter_unit_exercise_attempt_path(
+          basic_params.merge(exercise_id: exercise.id)
+        )
+      end
+
+      it 'will redirect to the first unit exercise that has not yet been validated even if another one is validated' do
+        create(:attempt, user: user, exercise: exercise, attempted_answer: exercise.answer)
+
+        response = get :new, params: basic_params.merge(exercise_id: exercise_3.id)
+        expect(response).to redirect_to new_course_chapter_unit_exercise_attempt_path(
+          basic_params.merge(exercise_id: exercise_2.id)
+        )
+      end
+
+      it 'will render good view if it is the first exercise' do
+        expect(Attempt).to receive(:new)
+        response = get :new, params: basic_params.merge(exercise_id: exercise.id)
+        expect(response.code).to eql('200')
+      end
+
+      it 'will render good view if previous exercise has been validated' do
+        create(:attempt, user: user, exercise: exercise, attempted_answer: exercise.answer)
+
+        expect(Attempt).to receive(:new)
+        response = get :new, params: basic_params.merge(exercise_id: exercise_2.id)
+
+        expect(response.code).to eql('200')
+      end
+
+      it 'will render good view if all previous exercises have been validated' do
+        create(:attempt, user: user, exercise: exercise, attempted_answer: exercise.answer)
+        create(:attempt, user: user, exercise: exercise_2, attempted_answer: exercise_2.answer)
+
+        expect(Attempt).to receive(:new)
+        response = get :new, params: basic_params.merge(exercise_id: exercise_3.id)
+        expect(response.code).to eql('200')
+      end
     end
-
-    it 'will redirect to the first unit exercise that has not yet been validated even if another one is validated' do
-      create(:attempt, user: user, exercise: exercise, attempted_answer: exercise.answer)
-
-      response = get :new, params: basic_params.merge(exercise_id: exercise_3.id)
-      expect(response).to redirect_to new_course_chapter_unit_exercise_attempt_path(
-        basic_params.merge(exercise_id: exercise_2.id)
-      )
-    end
-
-    it 'will render good view if it is the first exercise' do
-      response = get :new, params: basic_params.merge(exercise_id: exercise.id)
-      expect(response.code).to eql('200')
-    end
-
-    it 'will render good view if previous exercise has been validated' do
-      create(:attempt, user: user, exercise: exercise, attempted_answer: exercise.answer)
-      response = get :new, params: basic_params.merge(exercise_id: exercise_2.id)
-
-      expect(response.code).to eql('200')
-    end
-
-    it 'will render good view if all previous exercises have been validated' do
-      create(:attempt, user: user, exercise: exercise, attempted_answer: exercise.answer)
-      create(:attempt, user: user, exercise: exercise_2, attempted_answer: exercise_2.answer)
-
-      response = get :new, params: basic_params.merge(exercise_id: exercise_3.id)
-      expect(response.code).to eql('200')
-    end
-
-    # it 'in case of a post, it will save attempt && redirect to same exercise if it is a wrong answer' do
-    # end
-    #
-    # it 'will redirect to the next exercise if attempt is successful' do
-    # end
   end
 end
