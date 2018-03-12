4d62704b6a2de55cbb0cd034c88923b406bb09f6
Pierre Hersant <phersant@protonmail.com>
2018-03-12 23:45:57 +0100

    better display for learning elements

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,81 +13,81 @@ chapter_1 = Chapter.create(
 )
 
 unit_1 = Unit.create(
-  chapter: chapter,
+  chapter: chapter_1,
   title_en: 'Definition and characteristics of a set',
   position_in_chapter: 1
 )
 
 lesson_1 = Lesson.create(
-  unit: unit,
-  content_en: "A set can be defined as a collection, or a grouping of" \
+  unit: unit_1,
+  content_en: "A set can be defined as a collection, or a grouping of " \
     'elements;' \
-    "\n there are two ways to define sets :" \
-    "<ol>" \
-      "<li>" \
-        "by extension: write down all elements of the set between braces," \
-        "each element separated by a semicolon:" \
-        "for instance R = { 1 ; 2 ; 3 ; 4 }" \
-      "</li>" \
-      "<li>" \
-        "by comprehension: write down the conditions under which an" \
-        "element can be part of a set:" \
-        "\n each element must be put between simple braces: -> (x − 1)" \
-        "you put them one after the other -> (x − 1)(x − 2)(x − 3)(x − 4) = 0" \
-        "\n and you define a number x that belongs (belonging symbol : ∈)" \
-        "to a set R `such as` these conditions are respected." \
-        "\n The mathematical symbol for `such as` is written with the" \
-        "pipe : |" \
-        "\n { x ∈ R | (x − 1)(x − 2)(x − 3)(x − 4) = 0 }" \
-        "\n the mathematical expression above can be read as the" \
-        "following : `x belongs to the set R such as x - 1, or x - 2," \
-        "or x - 3, or x - 4 is equal to 0`" \
-        "\n Therefore you can easily conclude that R is a set" \
-        "that includes the numbers 1, 2, 3 and 4" \
-      "</li>" \
-    "</ol>"
+    "\n there are two ways to define sets : " \
+    "<ol> " \
+      "<li> " \
+        "by extension: write down all elements of the set between braces, " \
+        "each element separated by a semicolon: " \
+        "for instance R = { 1 ; 2 ; 3 ; 4 } " \
+      "</li> " \
+      "<li> " \
+        "by comprehension: write down the conditions under which an " \
+        "element can be part of a set: " \
+        "\n each element must be put between simple braces: -> (x − 1) " \
+        "you put them one after the other -> (x − 1)(x − 2)(x − 3)(x − 4) = 0 " \
+        "\n and you define a number x that belongs (belonging symbol : ∈) " \
+        "to a set R `such as` these conditions are respected. " \
+        "\n The mathematical symbol for `such as` is written with the " \
+        "pipe : | " \
+        "\n { x ∈ R | (x − 1)(x − 2)(x − 3)(x − 4) = 0 } " \
+        "\n the mathematical expression above can be read as the " \
+        "following : `x belongs to the set R such as x - 1, or x - 2, " \
+        "or x - 3, or x - 4 is equal to 0` " \
+        "\n Therefore you can easily conclude that R is a set " \
+        "that includes the numbers 1, 2, 3 and 4 " \
+      "</li> " \
+    "</ol>",
   position_in_unit: 1
 )
 
 example_1 = Example.create(
-  unit: unit,
-  content_en: "let E be the set that includes all even numbers below 10." \
-    "E = { 2 ; 4 ; 6 ; 8 }" \
-    "\n or you can define E by comprehension:" \
+  unit: unit_1,
+  content_en: "let E be the set that includes all even numbers below 10. " \
+    "E = { 2 ; 4 ; 6 ; 8 } " \
+    "\n or you can define E by comprehension: " \
     "\n { x ∈ E | (x / 2 is an integer) AND (x <= 10) }",
-  position_in_unit: 3
+  position_in_unit: 2
 )
 
 example_2 = Example.create(
-  unit: unit,
-  content_en: "let U be the set that includes all uneven numbers below 10." \
-    "U = { 1 ; 3 ; 5 ; 7 ; 9 }" \
-    "\n or we can define U by comprehension:" \
+  unit: unit_1,
+  content_en: "let U be the set that includes all uneven numbers below 10. " \
+    "U = { 1 ; 3 ; 5 ; 7 ; 9 } " \
+    "\n or we can define U by comprehension: " \
     "\n { x ∈ U | (x / 2 is not an integer) AND (x <= 10) }",
   position_in_unit: 3
 )
 
 exercise = Exercise.create(
-  unit: unit,
-  question_en: "Let R be a set which can be defined as follows:" \
-    "\n { x ∈ R | (x >= 5) AND (x < 10) AND (x is an integer) }" \
+  unit: unit_1,
+  question_en: "Let R be a set which can be defined as follows: " \
+    "\n { x ∈ R | (x >= 5) AND (x < 10) AND (x is an integer) } " \
     "\n define R by extension",
   answer: "{ 5 ; 6 ; 7 ; 8 ; 9 }",
   position_in_unit: 1
 )
 
 exercise_2 = Exercise.create(
-  unit: unit,
-  question_en: "Let S be a set which can be defined by extension as follows:" \
-    "\n S = { 1 ; 2 ; 3 ; 4 ; 5 ; 6 }" \
-    "\n can S be also defined by comprehension as follows?" \
+  unit: unit_1,
+  question_en: "Let S be a set which can be defined by extension as follows: " \
+    "\n S = { 1 ; 2 ; 3 ; 4 ; 5 ; 6 } " \
+    "\n can S be also defined by comprehension as follows? " \
     "\n { x ∈ S | (x <= 6) AND ((x + 1) > 6) }",
   answer: 'false',
-  position_in_unit: 1
+  position_in_unit: 2
 )
 
 # image = Image.create(
-#   unit: unit,
+#   unit: unit_1,
 #   img_url: 'https://picsum.photos/200/300',
 #   position_in_unit: 2
 # )
