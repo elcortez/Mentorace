b5eaa3e90c76acb929da6ba0304605e3ddd397a7
Pierre Hersant <phersant@protonmail.com>
2018-03-19 19:32:27 +0100

    first lesson : sets !

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,75 +16,103 @@ lesson_1 = Lesson.create(
   chapter: chapter_1,
   position_in_chapter: 1,
   title_en: 'Definition and characteristics of a set',
-  content_en: "<p> A set can be defined as a collection, or a grouping of " \
-    'elements; </p>' \
-    "<p> there are two ways to define sets : " \
-    "<ol> " \
-      "<li> " \
-        "by extension: write down all elements of the set between braces, " \
-        "each element separated by a semicolon: " \
-        "for instance R = { 1 ; 2 ; 3 ; 4 } " \
-      "</li> " \
-      "<li> " \
-        "by comprehension: write down the conditions under which an " \
-        "element can be part of a set: " \
-        "\n each element must be put between simple braces: -> (x − 1) " \
-        "you put them one after the other -> (x − 1)(x − 2)(x − 3)(x − 4) = 0 " \
-        "\n and you define a number x that belongs (belonging symbol : ∈) " \
-        "to a set R `such as` these conditions are respected. " \
-        "\n The mathematical symbol for `such as` is written with the " \
-        "pipe : | " \
-        "\n { x ∈ R | (x − 1)(x − 2)(x − 3)(x − 4) = 0 } " \
-        "\n the mathematical expression above can be read as the " \
-        "following : `x belongs to the set R such as x - 1, or x - 2, " \
-        "or x - 3, or x - 4 is equal to 0` " \
-        "\n Therefore you can easily conclude that R is a set " \
-        "that includes the numbers 1, 2, 3 and 4 " \
-      "</li> " \
-    "</ol> </p>"
+  content_en: "A set is a collection, a grouping of elements. \n\n" \
+    "There are two ways to define a set \n\n" \
+    "* by extension : R = { x ; y ; z } \n\n" \
+    "* by comprehension : { x ∈ R | (condition 1) AND (condition 2) = 0 } \n\n" \
+    "* the above sentence can be read as : `x belongs to R such as (condition 1) AND (condition 2)` \n\n" \
+    "(note that the ∈ symbol reads as `belongs to` and the pipe symbol | reads as `such as`) \n\n" \
+    "* An empty set is written with the symbol `∅` or with impossible conditions \n\n\n" \
+    "The cardinal of a set is the number of elements that it contains, \n\n" \
+    "and is usually written card(E) or #(E) \n\n" \
+    "Note that the cardinal of an empty set is obviously 0."
 )
 
 example_1 = Example.create(
   lesson: lesson_1,
-  content_en: "let E be the set that includes all even numbers below 10. " \
-    "E = { 2 ; 4 ; 6 ; 8 } " \
-    "\n or you can define E by comprehension: " \
-    "\n { x ∈ E | (x / 2 is an integer) AND (x <= 10) }",
+  content_en: "let E be the set that includes all even numbers below 10 and above 0. \n" \
+    "We can write E in two different manners \n\n" \
+    "* extension : E = { 2 ; 4 ; 6 ; 8 } \n\n" \
+    "* comprehension: { x ∈ E | (x is even) AND (x <= 10) AND (x > 0) }",
   position_in_lesson: 2
 )
 
 example_2 = Example.create(
   lesson: lesson_1,
-  content_en: "let U be the set that includes all uneven numbers below 10. " \
-    "U = { 1 ; 3 ; 5 ; 7 ; 9 } " \
-    "\n or we can define U by comprehension: " \
-    "\n { x ∈ U | (x / 2 is not an integer) AND (x <= 10) }",
+  content_en: "let F be the set that includes all uneven numbers below 10. \n" \
+    "We can write U in two different manners \n\n" \
+    "* extension : U = { 1 ; 3 ; 5 ; 7 ; 9 } \n\n" \
+    "* comprehension : { x ∈ U | (x is not even) AND (x <= 10) }",
   position_in_lesson: 3
+)
+
+example_3 = Example.create(
+  lesson: lesson_1,
+  content_en: "let G be an empty set \n" \
+    "We can write G in two different manners \n\n" \
+    "* extension : U = ∅ \n\n" \
+    "* comprehension : { x ∈ U | (x ** x) = -1 }",
+  position_in_lesson: 4
+)
+
+example_4 = Example.create(
+  lesson: lesson_1,
+  content_en: "let G be a set defined as follows : G = { 1 ; 2 ; 3} \n" \
+    "G's cardinal is 3 because it has 3 elements \n\n" \
+    "* card(G) = 3 \n\n" \
+    "* #(G) = 3",
+  position_in_lesson: 5
 )
 
 exercise_1 = Exercise.create(
   lesson: lesson_1,
-  question_en: "Let R be a set which can be defined as follows: " \
-    "\n { x ∈ R | (x >= 5) AND (x < 10) AND (x is an integer) } " \
-    "\n define R by extension",
+  question_en: "Let R be a set which can be defined as follows: \n" \
+    "{ x ∈ R | (x >= 5) AND (x < 10) AND (x is an integer) } \n" \
+    "define R by extension",
   answer: "{ 5 ; 6 ; 7 ; 8 ; 9 }",
   position_in_lesson: 1
 )
 
 exercise_2 = Exercise.create(
   lesson: lesson_1,
-  question_en: "Let S be a set which can be defined by extension as follows: " \
-    "\n S = { 1 ; 2 ; 3 ; 4 ; 5 ; 6 } " \
-    "\n can S be also defined by comprehension as follows? " \
-    "\n { x ∈ S | (x <= 6) AND ((x + 1) > 6) }",
+  question_en: "Let S be a set which can be defined by extension as follows: \n" \
+    "S = { 1 ; 2 ; 3 ; 4 ; 5 ; 6 } \n" \
+    "can S be also defined by comprehension as follows? \n" \
+    "{ x ∈ S | (x <= 6) AND ((x + 1) > 6) }",
   answer: 'false',
   position_in_lesson: 2
 )
 
-# lesson_2 = Lesson.create(
-#   content_en: "An empty set is defined as a set that contains no element." \
-#     "\n You can write it with the symbol : ∅ " \
-#     "\n Or you can write it by comprehension with an impossible equation : " \
-#     "\n { x ∈ R | x ** x = -1 }",
-#   position_in_lesson: 2
-# )
+exercise_3 = Exercise.create(
+  lesson: lesson_1,
+  question_en: "Let T be a set defined as follows: " \
+    "\n { x ∈ T | (x > 10) AND (x < 1) } " \
+    "\n is T an empty set?",
+  answer: 'true',
+  position_in_lesson: 3
+)
+
+exercise_4 = Exercise.create(
+  lesson: lesson_1,
+  question_en: "Let U be a set defined as follows: " \
+    "\n { x ∈ U | (x > 10) AND (x < 15) AND (x is an integer) } " \
+    "\n how many elements does U have?",
+  answer: '4',
+  position_in_lesson: 4
+)
+
+exercise_5 = Exercise.create(
+  lesson: lesson_1,
+  question_en: "Let V be a set defined as follows: " \
+    "\n { x ∈ V | (x ** x) = -1} " \
+    "\n what is the cardinal of V?",
+  answer: '0',
+  position_in_lesson: 5
+)
+
+#############################################################
+
+user = User.create(
+  email: 'pierre.hersant@gmail.com',
+  password: 'azertyuiopmlkjhgffdsq'
+)
