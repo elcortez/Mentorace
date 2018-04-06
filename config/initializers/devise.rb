c49a7c60e4f4c73cf07778ef410ca60eb2f10d5a
Pierre Hersant <phersant@protonmail.com>
2018-04-06 21:46:24 +0200

    rename additional titles

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'contact@folk-institute.com'
+  config.mailer_sender = 'contact@martial_teachings.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
