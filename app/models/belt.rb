9ef3d84dfa1eb57d36acc174335ba4c2750bd5aa
Pierre Hersant <phersant@protonmail.com>
2018-05-16 22:35:43 +0200

    add MAX XP constant

--- app/models/belt.rb
+++ app/models/belt.rb
@@ -5,4 +5,26 @@ class Belt < ApplicationRecord
   LEVELS = [*1..17]
 
   validates :level, inclusion: { in: LEVELS }
+
+  # TODO Find a better way to handle this
+  MAX_XP = {
+    1 => 10,
+    2 => 20,
+    3 => 30,
+    4 => 40,
+    5 => 45,
+    6 => 60,
+    7 => 70,
+    8 => 70,
+    9 => 70,
+    10 => 70,
+    11 => 70,
+    12 => 90,
+    13 => 90,
+    14 => 95,
+    15 => 110,
+    16 => 110,
+    17 => 150
+  }
+
 end
